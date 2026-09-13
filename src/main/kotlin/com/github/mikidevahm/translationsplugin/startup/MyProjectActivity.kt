4353c042a8708c051003d528a15ebeb02480a57d
GitHub Action <action@github.com>
2026-09-13 12:19:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/mikidevahm/translationsplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/mikidevahm/translationsplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.mikidevahm.translationsplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
